010b3bec3ad2fba19ff81dc95e641e01d22eecdd
filipeoliveira-oss <filipeoliveira084@gmail.com>
2024-05-20 22:12:45 -0300

    feat: add component MultiLine text box

---
+++ src/components/AudiMultiLine/AudiMultiLine.tsx
@@ -0,0 +1,10 @@
+import './AudiMultiLine.css'
+
+export default function AudiMultiLine(){
+    return(
+        <div className="AudiMultiLineContainer">
+            <textarea name="MultiLine" id="MultiLine" cols={30} rows={3} className='AudiMultiLineTextArea'></textarea>
+            <label htmlFor="MultiLine" className='AudiMultiLineLabel'>Label</label>
+        </div>
+    )
+}
