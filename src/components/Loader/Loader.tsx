1debbdb66f1f887d8a4cfee2bd4d7432084672e1
filipeoliveira-oss <filipeoliveira084@gmail.com>
2024-05-20 22:12:58 -0300

    feat: add spinner loader

---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,63 @@
+import "./Loader.css";
+import { useEffect, useState } from "react";
+
+interface loaderInterface{
+    loadTime: number
+}
+
+
+export default function Loader({loadTime} : loaderInterface) {
+
+    const [ loaded, setLoaded] = useState<number>(0)
+    const [loadPercent, setLoadPercent] = useState(0)
+
+    useEffect(() =>{
+        const maxVal = 280;
+        const intervalTime = loadTime / maxVal;
+        let currentValue = 0;
+
+        const intervalId = setInterval(() =>{
+            if(currentValue > maxVal){
+                clearInterval(intervalId)
+            }else{
+                setLoaded(currentValue)
+                currentValue++
+            }
+        },intervalTime)
+    },[])
+
+    useEffect(() =>{
+        setLoadPercent(Math.floor((100 * loaded) / 280))
+    },[loaded])
+
+    return (
+        <div className="LoaderContainer">
+            <svg className="aui-spinner__svg" viewBox="-46 -46 92 92">
+
+                <g className="aui-spinner__group" style={{strokeOpacity:1,strokeWidth:'4px', strokeDasharray: '300, 279.602', fill:'none',stroke:'#E0E0E0'}}>
+                    <g className="aui-spinner__group-base">
+                        <circle
+                            className="aui-spinner__path"
+                            cx="0"
+                            cy="0"
+                            r="44.5"
+                        ></circle>
+                    </g>
+                    {/* inner */}
+                    <g className="aui-spinner__group-progress">
+                        <circle
+                            className="aui-spinner__path aui-spinner__path--progress"
+                            cx="0"
+                            cy="0"
+                            r="44.5"
+                            transform="rotate(-90)"
+                            style={{strokeOpacity:1, strokeDasharray: `${loaded}, 279.602`, stroke:'#f50537', strokeWidth:'4px', fill:'none'}}
+                        ></circle>
+                    </g>
+                </g>
+            </svg>
+
+            <h3 className="LoaderPercent">{loadPercent}%</h3>
+        </div>
+    );
+}
